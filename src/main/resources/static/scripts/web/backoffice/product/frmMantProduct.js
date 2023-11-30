6c8d75069bbe484480dde498884e04a24d909392
EGEnrique181222 <sm75310865@idat.edu.pe>
2023-11-29 22:40:35 -0500

    mantenimiento final

--- src/main/resources/static/scripts/web/backoffice/product/frmMantProduct.js
+++ src/main/resources/static/scripts/web/backoffice/product/frmMantProduct.js
@@ -17,12 +17,35 @@ $(document).on("click", ".btnactualizar", function(){
     $("#cbocategoria").empty();
     $("#cboproveedor").empty();
     $("#switchproducto").show();
-    listarCategoriasProveedores($(this).attr("data-prodcateg"),$(this).attr("data-prodprov"));
+    listarCategoriasProveedores($(this).attr("data-prodcateg"), $(this).attr("data-prodprov"));
     if($(this).attr("data-descontinuado") === "true")
         $("#chkdescontinuado").prop("checked", true);
     else
         $("#chkdescontinuado").prop("checked", false);
     $("#modalNuevo").modal("show");
+});
+
+$(document).on("click", "#btnguardar", function(){
+    $.ajax({
+        type: "POST",
+        url: "/backoffice/product/guardar",
+        contentType: "application/json",
+        data: JSON.stringify({
+            productid: $("#hddcodprod").val(),
+            productname: $("#txtnomproducto").val(),
+            unitprice: $("#txtpreciounit").val(),
+            categoryid: $("#cbocategoria").val(),
+            supplierid: $("#cboproveedor").val(),
+            discontinued: $("#chkdescontinuado").prop("checked")
+        }),
+        success: function(resultado){
+            if(resultado.respuesta){
+                listarProductos()
+            }
+            alert(resultado.mensaje);
+            $("#modalNuevo").modal("hide");
+        }
+    })
 });
 
 function listarCategoriasProveedores(idcate, idprov){
@@ -36,7 +59,7 @@ function listarCategoriasProveedores(idcate, idprov){
                 `<option value="${value.categoryid}">${value.categoryname}</option>`
                 )
             })
-            if(idcate > 0){
+            if(idcate > 0) {
                 $("#cbocategoria").val(idcate);
             }
             $.ajax({
@@ -49,11 +72,39 @@ function listarCategoriasProveedores(idcate, idprov){
                             `<option value="${value.supplierid}">${value.companyname}</option>`
                             )
                         })
-                        if(idprov > 0){
+                        if(idprov > 0) {
                             $("#cboproveedor").val(idprov);
                         }
                     }
                 })
+        }
+    })
+}
+
+function listarProductos(){
+    $.ajax({
+        type: "GET",
+        url: "/backoffice/product/listar",
+        dataType: "json",
+        success: function(resultado){
+            $("#tblproducto > tbody").html("");
+            $.each(resultado, function(index, value){
+                $("#tblproducto > tbody").append("<tr>"+
+                    "<td>"+value.productid+"</td>"+
+                    "<td>"+value.productname+"</td>"+
+                    "<td>"+value.unitprice+"</td>"+
+                    "<td>"+value.category.categoryname+"</td>"+
+                    "<td>"+value.supplier.companyname+"</td>"+
+                    "<td>"+
+                        "<button type='button' class='btn btn-info btnactualizar'" +
+                            " data-prodcod='"+value.productid+"'"+
+                            " data-prodname='"+value.productname+"'"+
+                            " data-produnit='"+value.unitprice+"'"+
+                            " data-prodcateg='"+value.category.categoryid+"'"+
+                            " data-prodprov='"+value.supplier.supplierid+"'"+
+                            "><i class='fas fa-edit'></i></button></td></tr>"
+                );
+            })
         }
     })
 }
